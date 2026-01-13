5b125b3465d571f62465469c614e5242f7338c67
Samuele-Finotello <samuelefinotello.147@gmail.com>
2026-01-13 19:20:34 +0100

    [add] feat end snack 2

--- functions/snacks.js
+++ functions/snacks.js
@@ -3,6 +3,11 @@ function getInitials(fullName) {
   return `${name[0]}${surname[0]}`;
 }
 
+function createSlug(string) {
+  return string.toLowerCase();
+}
+
 module.exports = {
-  getInitials
+  getInitials,
+  createSlug
 }
--- tests/snacks.test.js
+++ tests/snacks.test.js
@@ -1,6 +1,12 @@
-const { getInitials } = require('../functions/snacks')
+const { getInitials, createSlug } = require('../functions/snacks')
 
 // SNACK 1
 test('La funzione getInitials restituisce le iniziali di un nome completo.', () => {
   expect(getInitials('Mario Rossi')).toBe('MR')
+})
+
+// SNACK 2
+test('La funzione createSlug restituisce una stringa in lowercase.', () => {
+  expect(createSlug('Cane')).toBe('cane')
+  expect(createSlug('GaTtO')).toBe('gatto')
 })
